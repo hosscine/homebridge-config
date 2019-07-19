a41134f9dec7272fd3ac4525663d39bb4e5f9461
hosscine <futagamirentaro@gmail.com>
2019-07-19 19:17:18 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp")
+const replace = require("gulp-replace")
+const env = require("gulp-env")
+
+env({
+  file: '.env.json'
+})
+
+gulp.task("replace", () => {
+  return gulp.src("config.json")
+    .pipe(replace("ENV.username", process.env.username))
+    .pipe(replace("ENV.port", process.env.port))
+    .pipe(replace("ENV.pin", process.env.pin))
+    .pipe(gulp.dest("build"))
+})
